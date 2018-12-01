c244ee675cadb597cc3345a65428d87952c123e5
H09bdavies <h09bryndavies@gmail.com>
2018-12-01 18:00:47 +0000

    more stuff

--- Red-DiscordBot/cogs/advent.py
+++ Red-DiscordBot/cogs/advent.py
@@ -2,6 +2,14 @@ import discord
 from discord.ext import commands
 from random import randint
 from random import choice
+import datetime
+
+try: # check if BeautifulSoup4 is installed
+    from bs4 import BeautifulSoup
+    soupAvailable = True
+except:
+    soupAvailable = False
+import aiohttp
 
 class Advent:
     """My custom cog that does stuff!"""
@@ -10,6 +18,7 @@ class Advent:
         self.bot = bot
         self.pref = ["Merry", "Happy", "Have a spooky"]
         self.suff = ["Crimbo", "Christmas", "New Year", "Easter", "Halloween", "Birthday"]
+        self.nice = [True, False]
 
     @commands.command()
     async def epic(self):
@@ -32,6 +41,45 @@ class Advent:
         #Your code will go here
         await self.bot.say(choice(self.pref) + " " + choice(self.suff))
 
+    @commands.command()
+    async def advent(self):
+        """advent"""
+
+        #Your code will go here
+        now = datetime.datetime.now()
+
+        if now.month == 12 and now.day < 26:
+            await self.bot.say("It's day " + str(now.day) + ", that means it's " + str(25-now.day) + " days until Christmas!")
+
+    @commands.command()
+    async def NorN(self):
+        """Naughty or Nice?"""
+
+        #Your code will go here
+        dab = choice(self.nice)
+        if dab:
+            await self.bot.say("You've been nice!")
+        else:
+            await self.bot.say("You've been naughty (ಠ益ಠ)")
+
+    @commands.command()
+    async def test(self):
+        """Merry crimbo"""
+
+        #Your code will go here
+        url = "https://steamdb.info/app/570/graphs/" #build the web adress
+        async with aiohttp.get(url) as response:
+            soupObject = BeautifulSoup(await response.text(), "html.parser")
+        try:
+            online = soupObject.find(class_='home-stats').find('li').find('strong').get_text()
+            await self.bot.say(online + ' players are playing this game at the moment')
+        except:
+            await self.bot.say("Couldn't load amount of players. No one is playing this game anymore or there's an error.")
+
+
 
 def setup(bot):
-    bot.add_cog(Advent(bot))
+    if soupAvailable:
+        bot.add_cog(Advent(bot))
+    else:
+        raise RuntimeError("You need to run `pip3 install beautifulsoup4`")
